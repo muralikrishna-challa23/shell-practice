987d9e7f73dd8ff4b01cb85102206ecb6289c93d
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-20 07:00:38 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: How Suresh, How are you?"
+echo "Suresh: Hi Ramesh, I am good, How are you?"
+echo "Ramesh: What are yoLearning?"
+echo "Suresh: I am learning shell Ramesh"
