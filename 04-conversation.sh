4c345dcd8c65c9ecd56fd20847e5f367adc303fe
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-22 13:18:43 +0530

    conversation with variables

---
+++ 04-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump:: Welcome to alaska putin"
+echo "Putin:: Thank you Trump, Finally face to face"
+echo "Trump:: So, do we have a deal"
+echo "Putin:: Yes, If you make Ukraine joins Russia"
