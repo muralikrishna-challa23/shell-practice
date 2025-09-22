8f403439e88460ae48c3ce89a3946030df191716
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-22 12:57:02 +0530

    conversation with variables

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,7 +3,7 @@
 PERSON1=John
 PERSON2=Raheem
 
-echo "$PERSON1: How $PERSON2, How are you?"
+echo "$PERSON1: Hellow $PERSON2, How are you?"
 echo "$PERSON2: Hi $PERSON1, I am good, How are you?"
-echo "$PERSON1: What are yoLearning?"
+echo "$PERSON1: What are you Learning?"
 echo "$PERSON2: I am learning shell $PERSON1"
