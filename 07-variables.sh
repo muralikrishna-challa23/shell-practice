a84ad4fa210a5ce18c7e4069e9e7e4c1bca7380f
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-22 15:44:45 +0530

    conversation with variables

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-DATE=$(date)
+#DATE=$(date)
 
-echo "Timestaamp Executed: $DATE"
+START_TIME=$(date +%s)
+sleep 10
+END_TIME=$(date +%s)
+
+echo "Timestaamp Executed: $TART_TIME-$END_TIME"
