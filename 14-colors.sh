c91db31c31eb85a2a96356d5f3b4bbaffada2a58
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-23 15:28:18 +0530

    conversation with variables

--- 14-colors.sh
+++ 14-colors.sh
@@ -1,45 +1,9 @@
 #!/bin/bash
 
-USERID=$(id -u)
 R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[37m"
 
-echo -e "$R Hellow World"
-
-
-if [ $USERID -ne 0 ]; then
-    echo "ERROR::Please run this with root ccess"
-    exit 1
- fi
-
-VALIDATE() {
-if [ $1 -ne 0 ]; then
-    echo "ERROR:: Installing $2 is FAILURE"
-    exit 1
- else
-    echo "Installing $2 is SUCCESS"   
- fi
-}
-
-dnf list installed mysql -y
-if [ $? -ne 0 ]; then
-    dnf install mysql -y
-    VALIDATE $? "MYSQL"
-else
-    echo "MYSQL is already installed"
-fi
-
-dnf list installed nginx -y
-if [ $? -ne 0 ]; then
-    dnf install nginx -y
-    VALIDATE $? "NGINX"
-else
-    echo "NGINX is already installed"
-fi
-
-dnf list installed mongodb-mongosh -y
-if [ $? -ne 0 ]; then
-    dnf install mongodb-mongosh -y
-    VALIDATE $? "MONGOSH"
-else
-    echo "MONGOSH is already installed"
-fi
+echo -e "$R Hellow World $W"
+echo "Check the Color"
---
+++ 15-colors.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+
+echo -e "$R Hellow World"
+echo "Check the Color"
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR::Please run this with root ccess"
+    exit 1
+ fi
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo "ERROR:: Installing $2 is FAILURE"
+    exit 1
+ else
+    echo "Installing $2 is SUCCESS"   
+ fi
+}
+
+dnf list installed mysql -y
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else
+    echo "MYSQL is already installed"
+fi
+
+dnf list installed nginx -y
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "NGINX"
+else
+    echo "NGINX is already installed"
+fi
+
+dnf list installed mongodb-mongosh -y
+if [ $? -ne 0 ]; then
+    dnf install mongodb-mongosh -y
+    VALIDATE $? "MONGOSH"
+else
+    echo "MONGOSH is already installed"
+fi
