4295a879797a72c4a8caf28d7b960a6f4e61af45
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-23 12:33:07 +0530

    conversation with variables

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,7 +3,7 @@
 echo "Please enter the value:"
 read NUMBER
 
-if [ $(($NUMBER % 2)) -eq 0 ]; then
+if [ $($NUMBER % 2) -eq 0 ]; then
     echo "The given number $NUMBER is EVEN"
 else
     echo "The given number $NUMBER is ODD"
