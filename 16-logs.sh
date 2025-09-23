bf8310139378808cdcccc7477debfcf2bb7a4ce0
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-23 15:40:14 +0530

    conversation with variables

---
+++ 16-logs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e "ERROR::Please run this with root ccess"
+    exit 1
+ fi
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo -e "ERROR:: Installing $2 ....$R FAILURE $W"
+    exit 1
+ else
+    echo -e "Installing $2 .....$G SUCCESS $W"   
+ fi
+}
+
+dnf list installed mysql -y
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else
+    echo -e "MYSQL is already installed ....$Y SKIPPING $W"
+fi
+
+dnf list installed nginx -y
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "NGINX"
+else
+    echo -e "NGINX is already installed ....$Y SKIPPING $W"
+fi
+
+dnf list installed mongodb-mongosh -y
+if [ $? -ne 0 ]; then
+    dnf install mongodb-mongosh -y
+    VALIDATE $? "MONGOSH"
+else
+    echo -e "MONGOSH is already installed ....$Y SKIPPING $W"
+fi
