5c15ce936a8d5dca7ea11372fde49fe25bf6be9e
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-23 14:51:43 +0530

    conversation with variables

--- 12-installation.sh
+++ 12-installation.sh
@@ -5,4 +5,14 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "ERROR::Please run this with root ccess"
     exit 1
- fi   
+ fi
+
+ dnf install mysql -y
+
+ if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing MYSQL is FAILURE"
+    exit 1
+ else
+    echo "Installing MYSQL is SUCCESS"   
+ fi
+       
