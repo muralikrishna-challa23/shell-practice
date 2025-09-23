23ed99575e339857c00ea70351c758d41242fa06
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-23 15:34:21 +0530

    conversation with variables

--- 15-colors.sh
+++ 15-colors.sh
@@ -2,9 +2,10 @@
 
 USERID=$(id -u)
 R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
 
-echo -e "$R Hellow World"
-echo "Check the Color"
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR::Please run this with root ccess"
@@ -13,10 +14,10 @@ if [ $USERID -ne 0 ]; then
 
 VALIDATE() {
 if [ $1 -ne 0 ]; then
-    echo "ERROR:: Installing $2 is FAILURE"
+    echo "ERROR:: Installing $2 ....$R FAILURE $W"
     exit 1
  else
-    echo "Installing $2 is SUCCESS"   
+    echo "Installing $2 .....$G SUCCESS $W"   
  fi
 }
 
@@ -25,7 +26,7 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y
     VALIDATE $? "MYSQL"
 else
-    echo "MYSQL is already installed"
+    echo "MYSQL is already installed ....$Y SKIPPING $W"
 fi
 
 dnf list installed nginx -y
@@ -33,7 +34,7 @@ if [ $? -ne 0 ]; then
     dnf install nginx -y
     VALIDATE $? "NGINX"
 else
-    echo "NGINX is already installed"
+    echo "NGINX is already installed ....$Y SKIPPING $W"
 fi
 
 dnf list installed mongodb-mongosh -y
@@ -41,5 +42,5 @@ if [ $? -ne 0 ]; then
     dnf install mongodb-mongosh -y
     VALIDATE $? "MONGOSH"
 else
-    echo "MONGOSH is already installed"
+    echo "MONGOSH is already installed ....$Y SKIPPING $W"
 fi
