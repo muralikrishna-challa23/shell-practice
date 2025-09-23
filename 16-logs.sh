e4250c728f58209b5343b0df755d245d3d2cb9f1
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-23 15:58:26 +0530

    conversation with variables

--- 16-logs.sh
+++ 16-logs.sh
@@ -10,20 +10,17 @@ LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME="$(echo $0 | cut -d '.' -f1)"
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
-echo "$LOG_FOLDER $SCRIPT_NAME $LOG_FILE"
-
-
 if [ $USERID -ne 0 ]; then
-    echo -e "ERROR::Please run this with root ccess"
+    echo -e "ERROR::Please run this with root ccess" | tee -a $LOG_FILE
     exit 1
  fi
 
 VALIDATE() {
 if [ $1 -ne 0 ]; then
-    echo -e "ERROR:: Installing $2 ....$R FAILURE $W"
+    echo -e "ERROR:: Installing $2 ....$R FAILURE $W" | tee -a $LOG_FILE
     exit 1
  else
-    echo -e "Installing $2 .....$G SUCCESS $W"   
+    echo -e "Installing $2 .....$G SUCCESS $W"    | tee -a $LOG_FILE
  fi
 }
 
@@ -32,7 +29,7 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y
     VALIDATE $? "MYSQL"
 else
-    echo -e "MYSQL is already installed ....$Y SKIPPING $W"
+    echo -e "MYSQL is already installed ....$Y SKIPPING $W" | tee -a $LOG_FILE
 fi
 
 dnf list installed nginx -y
@@ -40,7 +37,7 @@ if [ $? -ne 0 ]; then
     dnf install nginx -y
     VALIDATE $? "NGINX"
 else
-    echo -e "NGINX is already installed ....$Y SKIPPING $W"
+    echo -e "NGINX is already installed ....$Y SKIPPING $W" | tee -a $LOG_FILE
 fi
 
 dnf list installed mongodb-mongosh -y
@@ -48,5 +45,5 @@ if [ $? -ne 0 ]; then
     dnf install mongodb-mongosh -y
     VALIDATE $? "MONGOSH"
 else
-    echo -e "MONGOSH is already installed ....$Y SKIPPING $W"
+    echo -e "MONGOSH is already installed ....$Y SKIPPING $W" | tee -a $LOG_FILE
 fi
