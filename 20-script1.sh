4dd54d32cd504af66bd1f5952b3a3a2d19a571cd
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-27 11:09:03 +0530

    roboshop shell

---
+++ 20-script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NAME="INDIA"
+
+echo "My country name is: $NAME"
+echo "Script1 PID is: $$"
+
+sh 21-script2.sh
---
+++ 21-script2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "My country name is: $NAME"
+echo "Script2 PID is: $$"
