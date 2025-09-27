e34b8a2ad8e8551573a55286b85ed8b2e2a443da
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-27 22:16:24 +0530

    roboshop shell

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -9,3 +9,8 @@ echo "Who executed the script::$USER"
 echo "PID of script executed::$$"
 sleep 10 &
 echo "Last backgroud run command::$!"
+echo "Last executed command status::$?"
+echo "Number of command-line arguments::$#"
+echo "Internal Field Seperator, How shell splits strings into fields::$IFS"
+echo "Flags are set in the shell::$-"
+echo "Last argument provided to the previous command::$_"
