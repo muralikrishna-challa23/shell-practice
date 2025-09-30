174cfedc9e73d1148fe42bc3b5613387c23d1f36
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-30 21:40:47 +0530

    roboshop shell

--- 23-backup.sh
+++ 23-backup.sh
@@ -59,6 +59,7 @@ else
     FILES=$(find $SOURCE_DIR -name '*.log' -type f -mtime +$DAYS)
     VALIDATE $? "List of files found to archive."
     ZIP_NAME=$DEST_DIR/app-logs-$TIMESTAMP.zip
+    echo -e "zip file name is: $G $ZIP_NAME $W"
   #  find $SOURCE_DIR -name '*.log' -type f -mtime +$DAYS | zip 
 
 fi
