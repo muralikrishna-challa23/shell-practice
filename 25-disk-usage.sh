4c53cac8efca10dec84062118697e3925e3ce7c8
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-10-01 14:37:49 +0530

    roboshop shell

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -58,7 +58,7 @@ do
     PARTITION=$( echo $line | awk '{print $7}' )
 
     if [ $USAGE -gt $THRESHOLD ] ; then
-        MESSAGE+="Disk High Usage $USAGE% for partition: $PARTITION"
+        MESSAGE="Disk High Usage $USAGE% for partition: $PARTITION"
     fi
 
     echo $MESSAGE
